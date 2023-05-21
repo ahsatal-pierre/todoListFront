845ef63a356b951978958c58ef0435d89e4511dd
AHSATAL PIERRE <asathal.pierre@gmail.com>
2023-05-22 00:17:04 +0200

    second user story

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,57 @@
-import React from 'react';
+
+import React, { useState } from 'react';
 
 const TodoList = () => {
-  const todos = [
-    { id: 1, title: 'User story 1', state: 'pending' },
-    { id: 2, title: 'User story 2', state: 'completed' },
-    { id: 3, title: 'User story 3', state: 'pending' },
-  ];
+const [todos, setTodos] = useState([
+    { id: 1, title: 'User job 1', state: 'pending' },
+    { id: 2, title: 'User job 2', state: 'pending' },
+    { id: 3, title: 'User job 3', state: 'pending' },
+    { id: 4, title: 'User job 4', state: 'pending' },
+  ]);
+
+  // Change the state
+  const handleTodoStateChange = (todoId) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = prevTodos.map((todo) => {
+        if (todo.id === todoId) {
+          return {
+            ...todo,
+            state: todo.state === 'pending' ? 'completed' : 'pending',
+          };
+        }
+        return todo;
+      });
+
+      //Separate todos by states
+      const completedTodos = updatedTodos.filter(
+        (todo) => todo.state === 'completed'
+      );
+      const pendingTodos = updatedTodos.filter(
+        (todo) => todo.state === 'pending'
+      );
+
+      // Sort by state and by id 
+      const sortedPendingTodos = [...pendingTodos].sort((a, b) => a.id - b.id);
+      const sortedCompletedTodos = [...completedTodos].sort((a, b) => a.id - b.id);
+
+      return [...sortedPendingTodos, ...sortedCompletedTodos];
+    });
+  };
 
   return (
     <div>
+      <h1>Todo List</h1>
       <ol>
-        {todos.map((todo) => (
-          <li key={todo.id}>
-            {todo.title} - {todo.state}
+        {todos.map((todo) => ( <li
+            key={todo.id}
+            style={{ textDecoration: todo.state === 'completed' ? 'line-through' : 'none' }}
+          >
+            <input
+              type="checkbox"
+              checked={todo.state === 'completed'}
+              onChange={() => handleTodoStateChange(todo.id)}
+            />
+            {todo.title}
           </li>
         ))}
       </ol>
