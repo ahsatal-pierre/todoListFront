a7f70aff1b4e100fb59cd4c23053fee2f32ab758
AHSATAL PIERRE <asathal.pierre@gmail.com>
2023-05-23 03:18:49 +0200

    hashrouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 import App from "./App";
 
